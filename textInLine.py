2e220a725a42e58c62d20b1600c950c51a07d6e2
queque5987 <pyw5987@gmail.com>
2022-10-18 17:32:15 +0900

    20221018 1732 excel saving code

--- textInLine.py
+++ textInLine.py
@@ -2,6 +2,7 @@ import os
 import pathlib
 import json
 from tqdm import tqdm 
+import pandas as pd
 
 if __name__ == "__main__":
     file_name = "xlm_inferenced_c_event.txt"
@@ -41,12 +42,52 @@ if __name__ == "__main__":
                     print(inf[i].split())
                     print(org[i].split())
                     temp_entity.append([org[i], inf[i]])
+
+    """
+    엑셀 저장
+    """
+    # xlsx_entities = []
+    # for ent in tqdm(entity_hmap.values()):
+    #     temp = ent
+    #     while(True):
+    #         if len(temp) <= 3:
+    #             xlsx_entities.append(temp)
+    #             break
+    #         else:
+    #             xlsx_entities.append([temp[0], temp[-2], temp[-1]])
+    #             temp = temp[:-2]
+    # df = pd.DataFrame(xlsx_entities)
+    # df.to_excel("xlm_entities_to_analyse.xlsx", sheet_name= 'new_name', index= False, header= False)
+
+    """
+    txt 저장
+    """
+    # with open("xlm_entities_to_analyse.txt", "w", encoding= "utf-8") as file:
+    #     for entity in entity_hmap.values():
+    #         file.write("{}\n".format(entity))
+    # with open("xlm_entities_to_analyse_diff.txt", "w", encoding= "utf-8") as file:
+    #     for dat in temp_entity:
+    #         file.write("{}\n{}\n".format(dat[0], dat[1]))
     
-    with open("xlm_entities_to_analyse.txt", "w", encoding= "utf-8") as file:
-        # file = json.dump(entity_hmap, file)
-        for entity in entity_hmap.values():
-            file.write("{}\n-----\n".format(entity))
-    with open("xlm_entities_to_analyse_diff.txt", "w", encoding= "utf-8") as file:
-        for dat in temp_entity:
-            file.write("{}\n{}\n".format(dat[0], dat[1]))
+    per_entity = []
+    for entity in entity_hmap.values():
+        s = 0
+        d = 0
+        original_token = ''
+        for i, e in enumerate(entity):
+            if i == 0:
+                original_token = e
+                continue
+            if type(e) == int:
+                continue
+            if e == original_token:
+                s += entity[i+1]
+            else:
+                d += entity[i+1]
+        per_entity.append([original_token, d/(s+d)*100, d, s+d])
+    
+    print(per_entity[:20])
+    df = pd.DataFrame(per_entity)
+    df.to_excel("xlm_entities_to_analyse_recognized_as_NE_per_total.xlsx", sheet_name= 'new_name', index= False, header= False)
+
     print('done')
