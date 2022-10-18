78e7be379774dceead042e8fdc0d54f9ccd6615c
youngwoong Park <pyw5987@gmail.com>
2022-10-18 14:14:57 +0900

    20221018 1414 c_event inferenced upload

--- getData.py
+++ getData.py
@@ -1,22 +1,32 @@
 import os
 import pathlib
 import json
+from tqdm import tqdm
+import tfxlmrner40lang as xlm
 
-data_dir = pathlib.Path("방송대본요약/1.Training/원천데이터/TS1/enter")
-data = os.scandir(data_dir)
-for dat in data:
-    # print(dat)
-    # cate_dir = os.path.join(data_dir, dat)
-    for subcate in os.scandir(dat):
-        # for subcate in os.scandir(category):
-            print("----------------------------")
-            print(subcate.name)
+dat_categories = ["fm_drama", "fs_drama", "enter", "culture", "history"]
+txlm = xlm.tfxml()
+for dat_category in dat_categories:
+    print("processing --- {}".format(dat_category))
+    data_dir = pathlib.Path("방송대본요약/1.Training/원천데이터/TS1/{}".format(dat_category))
+    data = os.scandir(data_dir)
+    texts = []
+    for dat in data:
+        print(dat)
+        cate_dir = os.path.join(data_dir, dat)
+        for subcate in os.scandir(dat):
             with open(subcate, "r", encoding = 'utf-8') as file:
                 d_json = json.load(file)
-                # print(d_json['Meta']['passage'].strip())
                 dddd = d_json['Meta']['passage'].strip().split("\n")
                 for ddd in dddd:
-                    print(ddd)
-# print(dddd.strip())
-# for dd in dddd.split("\n"):
-#     print(dd)
+                    index = ddd.find(']')
+                    if (dat_category == "fm_drama" or dat_category == "fs_drama") and ddd[:index] == "해설":
+                        continue
+                    texts.append(ddd[index+1:])
+    print(texts[:10])
+    with open("xlm_inferenced_{}.txt".format(dat_category), "w", encoding='utf-8') as file:
+        for text in tqdm(texts):
+            file.write(txlm.inference(text))
+            file.write("\n")
+
+print("done")
