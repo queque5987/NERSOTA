7ec9d1f3aa3a8dc6d54701151466ec4650d595eb
queque5987 <pyw5987@gmail.com>
2022-12-05 23:19:54 +0900

    20221205 2319 inferencing robertaMLM

--- tfxlmrner40lang.py
+++ tfxlmrner40lang.py
@@ -30,6 +30,33 @@ class tfxml():
                 n += len(out['entity'])+2
         return temp_text
 
+class robertaMLM():
+    def __init__(self, trained = False):
+        from transformers import RobertaTokenizer, RobertaConfig, RobertaForMaskedLM, AutoTokenizer
+        from tokenizers import ByteLevelBPETokenizer
+        import torch
+        ckpt_dir = ""
+        if not trained:
+            self.tokenizer = AutoTokenizer.from_pretrained('BM-K/KoSimCSE-roberta')
+        else:
+            self.tokenizer = ByteLevelBPETokenizer(
+            'bpe/vocab.json',
+            'bpe/merges.txt'
+        )
+        with open(ckpt_dir + "config.json", "r") as file:
+            self.config = json.load(file)
+        self.model = RobertaForMaskedLM(self.config)
+        self.model.load_state_dict(torch.load(ckpt_dir + "*.bin"))
+        self.model.eval()
+        self.pipeline = pipeline(
+            "ner",
+            model=self.model,
+            tokenizer=self.tokenizer,
+            framework="pt"
+        )
+    def infernece(self, text):
+        return pipeline(text)
+
 class xlmr():
     def __init__(self):
         self.tokenizer = AutoTokenizer.from_pretrained("xlm-roberta-large-finetuned-conll03-english")
@@ -104,13 +131,17 @@ if __name__ == "__main__":
     # letr = letrAPI()
     import pandas as pd
     from tqdm import tqdm
-    # import time
-    # import random
+    import time
+    import random
     # ckpt_name = 'epoch=2-val_loss=0.06'
     # kcbert_model = kcbert.inference('C:/nlpbook/checkpoint-ner/{}.ckpt'.format(ckpt_name))
-    import kc_bert
     # test_dataset = pd.read_csv('corpus/new_corpus_no_overlap_no_drop_test_data_4_1109.csv', sep=',')
-    
+    # kcbert_model = kcbert()
+    kcbert_model = ftd_kcbert.inference(ckpt_dir="epoch=2-val_loss=0.14.ckpt", label_map_dir="label_map.txt")
+    j = "15rogowntpdy.json"
+    with open(j,'r', encoding='utf-8') as j_file:
+        lines = json.load(j_file)
+    # lines = []
     # lines = test_dataset['ko_original'].values.tolist()
     # print(len(lines))
     # total_output = []
@@ -128,33 +159,24 @@ if __name__ == "__main__":
     #     # print(total_output[i:i+5 if i+5 <= len(lines) else -1])
     #     if idx == len(lines): break
     # xlmr = xlmr()
-    kcbert_model = kc_bert.inference(ckpt_dir="epoch=2-val_loss=0.14.ckpt", label_map_dir="label_map.txt")
-    daily = "to_tagging_data/kor_eng_dlftkdtodghkf.json"
-    
-    def infer_kcbert(dir, out_dir):
-        with open(dir, 'r', encoding='utf-8') as j_file:
-            lines = json.load(j_file)
-        print(dir)
-        final_outputs = []
-        for line in tqdm(lines):
-            outputs = []
-            # lines.append(line.strip())
-            output = kcbert_model.inference_fn(line)
-            # output = kcbert_model.inference_fn(line)
-            # for result in output['result']:
-            #     outputs.append(result)
-            final_outputs.append({'sentence' : line, 'result' : output.get('result') if output.get('result') else output})
-            # print(outputs)
-            # break
-            # output = xlmr.inference(line)
-            # t, o, b = spcaye.inference(line)
-            # lines.append({'ko_original' : line, 'tokens' : t, 'output' : o, 'bio_tags' : b})
-            # break
-        with open(out_dir, "w", encoding='utf-8') as outfile:
-            json.dump(final_outputs, outfile, indent=2, ensure_ascii=False)
-    infer_kcbert("to_tagging_data/kor_eng_dlftkdtodghkf.json", "to_tagging_data/kor_eng_dlftkdtodghkf_predicted.json")
-    infer_kcbert("to_tagging_data/kor_eng_coxld.json", "to_tagging_data/kor_eng_coxld_predicted.json")
-    infer_kcbert("to_tagging_data/kor_eng_godhlduddjq.json", "to_tagging_data/kor_eng_godhlduqdjq_predicted.json")
+    final_outputs = []
+    for line in tqdm(lines):
+        outputs = []
+        # lines.append(line.strip())
+        output = kcbert_model.inference_fn(line)
+        # output = kcbert_model.inference_fn(line)
+        # for result in output['result']:
+        #     outputs.append(result)
+        final_outputs.append({'sentence' : line, 'ner' : output})
+        # print(outputs)
+        # break
+        # output = xlmr.inference(line)
+        # t, o, b = spcaye.inference(line)
+        # lines.append({'ko_original' : line, 'tokens' : t, 'output' : o, 'bio_tags' : b})
+        # break
+    # print(lines[:1])
+    with open("15rogowntpdy_predicted.json", "w", encoding='utf-8') as outfile:
+        json.dump(final_outputs, outfile, indent=2, ensure_ascii=False)
     # output = pd.DataFrame(final_outputs)
     # output.to_csv('output_kc_bert_{}_1110.csv'.format('ckpt_name'), index=False)
     # print('done')
